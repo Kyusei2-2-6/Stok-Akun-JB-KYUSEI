5552178e419c0dfdfc51173fa566e1213a5abada
Kyusei2-2-6 <maunyolongyaaaa@gmail.com>
2025-12-18 14:43:09 +0700

    Update data.js

--- data.js
+++ data.js
@@ -33,6 +33,7 @@ var PRODUCTS = [
     name: "Sempak Gusion",
     game: "ml",
     price: 100000000000,
+    sold: true,
     photos: [
       "assets/product/Inden.png"
     ],
@@ -47,6 +48,7 @@ function rupiah(n){
   return "Rp" + String(Math.trunc(num))
     .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
+
 
 
 
