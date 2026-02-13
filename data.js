fd3c3d5703b9d6414a01ad0f73efc26941d6ada6
Kyusei2-2-6 <maunyolongyaaaa@gmail.com>
2026-02-13 13:19:40 +0700

    Kyusei bot: ADD B002

--- data.js
+++ data.js
@@ -1,100 +1,114 @@
 var SITE_NAME = "Kyusei";
 var PRODUCTS = [
   {
-    code: "A001",
-    name: "Genshin A001",
-    game: "genshin",
-    price: 50000,
-    sold: false,
-    photos: [
+    "code": "A001",
+    "name": "Genshin A001",
+    "game": "genshin",
+    "price": 50000,
+    "sold": false,
+    "photos": [
       "assets/products/genshin/A001/1.jpg",
       "assets/products/genshin/A001/2.jpg",
       "assets/products/genshin/A001/3.jpg"
     ],
-    detail: [
+    "detail": [
       "Isi detail A001 di sini"
     ]
   },
   {
-    code: "A002",
-    name: "Genshin A002",
-    game: "genshin",
-    price: 75000,
-    sold: false,
-    photos: [
+    "code": "A002",
+    "name": "Genshin A002",
+    "game": "genshin",
+    "price": 75000,
+    "sold": false,
+    "photos": [
       "assets/products/genshin/A002/1.jpg",
       "assets/products/genshin/A002/2.jpg",
       "assets/products/genshin/A002/3.jpg",
       "assets/products/genshin/A002/4.jpg",
       "assets/products/genshin/A002/5.jpg"
     ],
-    detail: [
+    "detail": [
       "Isi detail A002 di sini"
     ]
   },
   {
-    code: "A003",
-    name: "Genshin A003",
-    game: "genshin",
-    price: 50000,
-    sold: false,
-    photos: [
+    "code": "A003",
+    "name": "Genshin A003",
+    "game": "genshin",
+    "price": 50000,
+    "sold": false,
+    "photos": [
       "assets/products/genshin/A003/1.jpg"
     ],
-    detail: [
+    "detail": [
       "Isi detail A003 di sini"
     ]
   },
   {
-    code: "A004",
-    name: "Genshin A004",
-    game: "genshin",
-    price: 50000,
-    sold: false,
-    photos: [
+    "code": "A004",
+    "name": "Genshin A004",
+    "game": "genshin",
+    "price": 50000,
+    "sold": false,
+    "photos": [
       "assets/products/genshin/A004/1.jpg",
       "assets/products/genshin/A004/2.jpg"
     ],
-    detail: [
+    "detail": [
       "Isi detail A004 di sini"
     ]
   },
   {
-    code: "A005",
-    name: "Genshin A005",
-    game: "genshin",
-    price: 50000,
-    sold: false,
-    photos: [
+    "code": "A005",
+    "name": "Genshin A005",
+    "game": "genshin",
+    "price": 50000,
+    "sold": false,
+    "photos": [
       "assets/products/genshin/A005/1.jpg",
       "assets/products/genshin/A005/2.jpg"
     ],
-    detail: [
+    "detail": [
       "Isi detail A005 di sini"
     ]
   },
-
   {
-    code: "B001",
-    name: "Mobile Legends B001",
-    game: "ml",
-    price: 0,
-    sold: false,
-    photos: [
+    "code": "B001",
+    "name": "Mobile Legends B001",
+    "game": "ml",
+    "price": 0,
+    "sold": false,
+    "photos": [
       "assets/products/ml/B001/1.png"
     ],
-    detail: [
+    "detail": [
       "Isi detail B001 di sini"
     ]
   },
-
   {
-    code: "C001",
-    name: "Toram C001",
-    game: "toram",
-    price: 0,
-    sold: false,
-    photos: [
+    "code": "B002",
+    "name": "P",
+    "game": "ml",
+    "price": 1,
+    "sold": false,
+    "photos": [
+      "assets/products/ml/B002/1.jpg"
+    ],
+    "detail": [
+      "Anjai",
+      "Woilaj",
+      "Wkanaj",
+      "Jaja"
+    ]
+  },
+  {
+    "code": "C001",
+    "name": "Toram C001",
+    "game": "toram",
+    "price": 0,
+    "sold": false,
+    "photos": [
       "assets/products/toram/C001/1.jpg",
       "assets/products/toram/C001/2.jpg",
       "assets/products/toram/C001/3.jpg",
@@ -105,7 +119,7 @@ var PRODUCTS = [
       "assets/products/toram/C001/8.jpg",
       "assets/products/toram/C001/9.jpg"
     ],
-    detail: [
+    "detail": [
       "Isi detail C001 di sini"
     ]
   }
