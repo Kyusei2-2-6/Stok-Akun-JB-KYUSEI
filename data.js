14b2e84d95fdfa274662d573f1f2966518956bcc
Kyusei2-2-6 <maunyolongyaaaa@gmail.com>
2026-04-08 10:00:35 -0700

    bot push updates

--- data.js
+++ data.js
@@ -165,7 +165,7 @@ var PRODUCTS = [
     "name": "Starter Zibai",
     "game": "genshin",
     "price": 50000,
-    "sold": false,
+    "sold": true,
     "photos": [
       "assets/products/genshin/A007/1.jpg",
       "assets/products/genshin/A007/2.jpg",
